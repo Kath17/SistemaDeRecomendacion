fa35cf0ff331c2ba96e96587fc9c9e052e6aa77b
Crhistian Turpo <no.mercy.cs573@gmail.com>
2019-05-23 23:31:06 -0500

    Some Changes
    asd

--- App/main.py
+++ App/main.py
@@ -3,14 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return render_template("index.html")
 
 @app.route("/prueba")
-
 def salvador():
     return render_template("prueba.html")
+
+@app.route("/ranking")
+def ranking():
+    return render_template("ranking.html")
+
+@app.route("/slopeOne")
+def slopeOne():
+    return render_template("slopeOne.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
